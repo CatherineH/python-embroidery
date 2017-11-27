1e866910711d47ed31ec9f6b4e24ee51d1a505d0
Catherine Holloway <milankie@gmail.com>
2017-11-27 07:20:14 -0500

    adding getting device by producit id

--- svg_to_stitch.py
+++ svg_to_stitch.py
@@ -1,53 +1,33 @@
-from os.path import join, dirname, abspath
+from os import statvfs, remove
+from os.path import join, abspath, basename, getsize, exists
+from shutil import copyfile
+from xml.etree import ElementTree
 
+import re
 from svgpathtools import svg2paths
 from sys import argv
-from webcolors import hex_to_rgb
 import csv
-from subprocess import call
+from subprocess import call, check_output
 
+tree = ElementTree.parse(argv[1])
+root = tree.getroot()
+# make sure the document size is appropriate
+assert root.attrib['width'] == '4in'
+assert root.attrib['height'] == '4in'
 
 paths, attributes = svg2paths(argv[1])
+lims = [float(i) for i in root.attrib['viewBox'].split(" ")]
 
-xlims = [None, None]
-ylims = [None, None]
-
-for path in paths:
-    for segment in path:
-        # xlimits
-        if segment.start.real >= xlims[1]:
-            xlims[1] = segment.start.real
-        if segment.end.real >= xlims[1]:
-            xlims[1] = segment.end.real
-        if xlims[0] is None:
-            xlims[0] = segment.start.real
-        if segment.start.real < xlims[0]:
-            xlims[0] = segment.start.real
-        if segment.end.real < xlims[0]:
-            xlims[0] = segment.end.real
-        # y limits
-        if segment.start.imag >= ylims[1]:
-            ylims[1] = segment.start.imag
-        if segment.end.imag >= ylims[1]:
-            ylims[1] = segment.end.imag
-        if ylims[0] is None:
-            ylims[0] = segment.start.imag
-        if segment.start.imag < ylims[0]:
-            ylims[0] = segment.start.imag
-        if segment.end.imag < ylims[0]:
-            ylims[0] = segment.end.imag
-
-print(ylims, xlims)
-
-width = abs(xlims[0] - xlims[1])
-height = abs(ylims[0] - ylims[1])
+width = abs(lims[0] - lims[2])
+height = abs(lims[1] - lims[3])
 
 # this script is in mms. The assumed minimum stitch width is 1 mm, the maximum width is 5 mm
-minimum_stitch = 5.0
-maximum_stitch = 5.0
+minimum_stitch = 15.0
+maximum_stitch = 15.0
 
 # The maximum size is 4 inches
 size = 4.0 * 25.4
+
 if width > height:
     scale = size / width
 else:
@@ -67,6 +47,8 @@ def distance(point1, point2):
 stitches = []
 
 for k, v in enumerate(attributes):
+    #if k > 0:
+    #    continue
     to = Stitch()
     # first, look for the color from the fill
     if v['style'].find('fill:') >= 0:
@@ -75,20 +57,37 @@ for k, v in enumerate(attributes):
         # the color is black
         to.color = '#000000'
     for i, path in enumerate(paths[k]):
-        t = 0.0
         if path.length() == 0:
             continue
+        if i > 0:
+            t = minimum_stitch / path.length()
+        else:
+            t = 0.0
+        if path.length() == 0:
+            continue
+        print(type(path))
         while t <= 1.0:
+            print(path.point(t).real * scale, height*scale-path.point(t).imag * scale)
             to.x.append(path.point(t).real * scale)
             to.y.append(height*scale-path.point(t).imag * scale)
-            t += minimum_stitch / path.length()
+            if str(type(path)).find("Line") >= 0:
+                t += 1.0
+                print(t)
+            else:
+                t += minimum_stitch / path.length()
+
     stitches.append(to)
 
 # get a unique list of thread colors
-colors = set([to.color for to in stitches])
-
+colors = list(set([to.color for to in stitches]))
+#colors = [colors[0]]
 # start writing the output file
-of = open(argv[1].replace('.svg', '.csv'), 'w')
+csv_filename = argv[1].replace('.svg', '.csv')
+if exists(csv_filename):
+    # this step is somewhat unnecessary, but I want to be absolutely sure that the new
+    # design is being uploaded
+    remove(csv_filename)
+of = open(csv_filename, 'w')
 csvwriter = csv.writer(of, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
 intro_lines = ["Embroidermodder 2 CSV Embroidery File",
                "http://embroidermodder.github.io",
@@ -99,9 +98,9 @@ for line in intro_lines:
 csvwriter.writerow(["#", "[VAR_NAME]", "[VAR_VALUE]"])
 
 vars = {"STITCH_COUNT": sum([len(st.x) - 1 for st in stitches]),
-        "THREAD_COUNT": len(colors), "EXTENTS_LEFT": xlims[0] * scale,
-        "EXTENTS_TOP": ylims[0] * scale, "EXTENTS_RIGHT": xlims[1] * scale,
-        "EXTENTS_BOTTOM": ylims[1] * scale, "EXTENTS_WIDTH": width * scale,
+        "THREAD_COUNT": len(colors), "EXTENTS_LEFT": lims[0] * scale,
+        "EXTENTS_TOP": lims[1] * scale, "EXTENTS_RIGHT": lims[2] * scale,
+        "EXTENTS_BOTTOM": lims[3] * scale, "EXTENTS_WIDTH": width * scale,
         "EXTENTS_HEIGHT": height * scale}
 
 for varkey in vars:
@@ -110,10 +109,11 @@ for varkey in vars:
 csvwriter.writerow(["#", "[THREAD_NUMBER]", "[RED]", "[GREEN]", "[BLUE]", "[DESCRIPTION]",
                     "[CATALOG_NUMBER]"])
 for i, color in enumerate(colors):
-    csvwriter.writerow(["$", i] + list(hex_to_rgb(color)) + ["(null)", "(null)"])
+    csvwriter.writerow(["$", i+1] + [0, 0, 0] + ["(null)", "(null)"])
 
 csvwriter.writerow(["#", "[STITCH_TYPE]", "[X]", "[Y]"])
 # stitches need to be sorted by color in order to minimize thread changes
+
 for i, color in enumerate(colors):
     stitches_by_col = [st for st in stitches if st.color == color]
     if i != 0:
@@ -127,11 +127,43 @@ for i, color in enumerate(colors):
     # if on last color, add end statement
     if i == len(colors)-1:
         csvwriter.writerow(["*", "END", stitches_by_col[-1].x[-1], stitches_by_col[-1].x[-1]])
+of.close()
 
 # convert the file to PES format
 full_filename = abspath(argv[1]).replace(".svg", ".csv")
+pes_filename = full_filename.replace(".csv", ".pes")
+if exists(pes_filename):
+    remove(pes_filename)
 print("CSV has been generated: ", full_filename)
 try:
-    call(["libembroidery-convert", full_filename, full_filename.replace(".csv", ".pes")])
+    call(["libembroidery-convert", full_filename, pes_filename])
 except OSError as e:
-    print("Could not find libembroidery-convert. Did you install embroiderymodder and add libembroidery-convert to your path?")
+    print("Could not find libembroidery-convert. Did you install embroiderymodder and "
+          "add libembroidery-convert to your path?")
+
+
+mount_points = {}
+out = check_output(["df"]).split("\n")
+for line in out:
+    if line.find("/media") >= 0:
+        mount_location = line.split("%")[-1]
+        line = line.split(" ")
+        mount_points[line[0]] = mount_location.strip()
+
+mount_destination = None
+for mount_point in mount_points:
+    out = check_output(["udevadm", "info", "--name="+mount_point, "--attribute-walk"])
+    if out.find('ATTRS{idVendor}=="04f9"')> 0:
+        print("found brother device, assuming embroidery machine")
+        mount_destination = mount_points[mount_point]
+        break
+
+
+if mount_destination is None:
+    print("could not find brother embroidery machine to transfer to")
+else:
+    statvfs = statvfs(mount_destination)
+    if getsize(pes_filename) > statvfs.f_frsize * statvfs.f_bavail:
+        print("file cannot be transfered - not enough space on device")
+    else:
+        copyfile(pes_filename, join(mount_destination, basename(pes_filename)))
